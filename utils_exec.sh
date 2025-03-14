1cbcaff83e2e795bff28232031bb5dce230cb0b9
Nathaniel Moschkin <ironywrit@gmail.com>
2025-03-14 19:47:45 -0400

    change to utils scripts.

--- utils_exec.sh
+++ utils_exec.sh
@@ -3,6 +3,7 @@ date
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR
 npm run start
-bash /utils/utils_minor.sh
+date | grep -v -e " 19:4" -e " 07:4" && ./utils_minor.sh
+date | grep -e " 19:4" -e " 07:4"  && ./utils_major.sh
 popd
 
