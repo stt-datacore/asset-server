ff2c16f8e77de7f5e0e9e8d7caad3cadba997814
Nathaniel Moschkin <ironywrit@gmail.com>
2025-03-14 19:49:14 -0400

    fix

--- utils_exec.sh
+++ utils_exec.sh
@@ -3,7 +3,7 @@ date
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR
 npm run start
-date | grep -v -e " 19:4" -e " 07:4" && ./utils_minor.sh
-date | grep -e " 19:4" -e " 07:4"  && ./utils_major.sh
+date | grep -v -e " 19:4" -v -e " 07:4" && /utils/utils_minor.sh
+date | grep -e " 19:4" -e " 07:4"  && /utils/utils_major.sh
 popd
 
