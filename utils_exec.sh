72c98cd11df9ee815670019a5e2aa19917252805
Nathaniel Moschkin <ironywrit@gmail.com>
2025-03-14 20:06:04 -0400

    UTC always works.

--- utils_exec.sh
+++ utils_exec.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-TZ=America/New_York date
+TZ=UTC date
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR
 npm run start
-TZ=America/New_York date | grep -v -e " 19:3" -v -e " 07:3" && /utils/utils_minor.sh
-TZ=America/New_York date | grep -e " 19:3" -e " 07:3" && /utils/utils_major.sh
+TZ=UTC date | grep -v -e " 00:0" -v -e " 12:0" && /utils/utils_minor.sh
+TZ=UTC date | grep -e " 00:0" -e " 12:0" && /utils/utils_major.sh
 popd
 
