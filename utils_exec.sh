3885455cd9febac8c9afccbc8a0aca1a9874a917
Nathaniel Moschkin <ironywrit@gmail.com>
2025-03-14 21:00:36 -0400

    Change run times

--- utils_exec.sh
+++ utils_exec.sh
@@ -3,7 +3,7 @@ TZ=UTC date
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR
 npm run start
-TZ=UTC date | grep -v -e " 00:3" -v -e " 12:3" && /utils/utils_minor.sh
-TZ=UTC date | grep -e " 00:3" -e " 12:3" && /utils/utils_major.sh
+TZ=UTC date | grep -v -e " 02:3" -v -e " 14:3" && /utils/utils_minor.sh
+TZ=UTC date | grep -e " 02:3" -e " 14:3" && /utils/utils_major.sh
 popd
 
