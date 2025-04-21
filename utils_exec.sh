bb4280f8d8c8ac90bfad087b6ae16a97ba2378f5
Nathaniel Moschkin <ironywrit@gmail.com>
2025-04-20 23:05:37 -0400

    add command option

--- utils_exec.sh
+++ utils_exec.sh
@@ -3,7 +3,7 @@ TZ=UTC date
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR
 npm run start
-if [ "$1" === "--first"]; then
+if [ "$1" = "--first" ]; then
     /utils/utils_minor.sh
 fi
 TZ=UTC date | grep -v -e " 02:3" -v -e " 14:3" && /utils/utils_minor.sh
