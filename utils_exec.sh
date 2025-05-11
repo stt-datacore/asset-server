baed8bd1517ac227f180433b110fbc73994118fa
Nathaniel Moschkin <ironywrit@gmail.com>
2025-05-11 17:08:20 -0400

    edit minor run condition

--- utils_exec.sh
+++ utils_exec.sh
@@ -6,7 +6,7 @@ npm run start
 if [ "$1" = "--first" ]; then
     /utils/utils_minor.sh
 fi
-TZ=UTC date | grep -v -e " 02:3" -v -e " 14:3" -v -e ":\w5:" && /utils/utils_minor.sh
+TZ=UTC date | grep -v -e " 02:3" -v -e " 14:3" && date | grep -e ":00:" -e ":30:" && /utils/utils_minor.sh
 TZ=UTC date | grep -e " 02:3" -e " 14:3" && /utils/utils_major.sh
 TZ=UTC date | grep -e ":\w5:" -e ":\w0:" && date | grep -v -e ":30:" -v -e ":00:" && /utils/utils_cap.sh
 
