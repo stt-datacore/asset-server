146cd4101910a44894e6d0e78cadc7756671169e
Nathaniel Moschkin <ironywrit@gmail.com>
2025-05-11 17:31:55 -0400

    update utils script to do FTM only (no assets)

--- utils_exec.sh
+++ utils_exec.sh
@@ -2,13 +2,14 @@
 TZ=UTC date
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR
+TZ=UTC date | grep -e ":\w5:" -e ":\w0:" && date | grep -v -e ":30:" -v -e ":00:" && /utils/utils_cap.sh && exit 0
 npm run start
 if [ "$1" = "--first" ]; then
     /utils/utils_minor.sh
 fi
 TZ=UTC date | grep -v -e " 02:30" -v -e " 14:30" && date | grep -e ":00:" -e ":30:" && /utils/utils_minor.sh
 TZ=UTC date | grep -e " 02:30" -e " 14:30" && /utils/utils_major.sh
-TZ=UTC date | grep -e ":\w5:" -e ":\w0:" && date | grep -v -e ":30:" -v -e ":00:" && /utils/utils_cap.sh
+
 
 popd
 
