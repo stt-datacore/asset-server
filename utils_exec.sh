66234f5418eb782f547a0957de8aa4b2e016bb19
Nathaniel Moschkin <ironywrit@gmail.com>
2025-06-11 13:38:03 -0400

    update scripts to run on first go.

--- utils_exec.sh
+++ utils_exec.sh
@@ -2,9 +2,11 @@
 TZ=UTC date
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR
-TZ=UTC date | grep -e ":\w5:" -e ":\w0:" && date | grep -v -e ":30:" -v -e ":00:" && /utils/utils_cap.sh && popd && exit 0
+if [ "$1" != "--first" ]; then
+    TZ=UTC date | grep -e ":\w5:" -e ":\w0:" && date | grep -v -e ":30:" -v -e ":00:" && /utils/utils_cap.sh && popd && exit 0
+fi
 npm run start
-if [ "$1" = "--first" ]; then
+if [ "$1" == "--first" ]; then
     /utils/utils_minor.sh
 fi
 TZ=UTC date | grep -v -e " 02:30" -v -e " 14:30" && date | grep -e ":00:" -e ":30:" && /utils/utils_minor.sh
